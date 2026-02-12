45c6024c0c3b98c7893c7debdbd827bf3d7efaf5
Lucas Vinicius <lcasgalindo@gmail.com>
2026-02-12 17:17:39 -0300

    starting the refactor

---
+++ sales-backend/src/Application/User/DTOs/UserResponse.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Application\Auth\DTOs;
+
+use App\Domain\User\Entity\User;
+use App\Domain\User\ValueObject\Email;
+
+class UserResponse
+{
+
+  public function __construct(
+    public readonly string $id,
+    public readonly int $tenant_id,
+    public readonly string $name,
+    public readonly Email $email,
+    public readonly string $email_verified_at,
+    public readonly string $password,
+    public readonly bool $isActive,
+  ) {
+  }
+
+  public static function fromEntity(User $user): self
+  {
+    return new self(
+      id: $user->getId(),
+      tenant_id: $user->getTenantId(),
+      name: $user->getName(),
+      email: $user->getEmail(),
+      email_verified_at: $user->getEamilVerifiedAt(),
+      password: $user->getPassword(),
+      isActive: $user->isIsActive(),
+    );
+  }
+
+  public function toArray()
+  {
+    return [
+      'id' => $this->id,
+      'tenant_id' => $this->tenant_id,
+      'name' => $this->name,
+      'email' => $this->email,
+      'email_verified_at' => $this->email_verified_at,
+      'password' => $this->password,
+      'isActive' => $this->isActive
+    ];
+  }
+
+}
